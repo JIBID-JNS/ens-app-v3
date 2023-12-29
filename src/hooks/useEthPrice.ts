75c1b7986d3105ae1e54b12f57a9493fdac124c7
AngryBird <angry.bird@users.noreply.github.com>
2023-12-29 10:29:21 +0700

    fixed lint error

--- src/hooks/useEthPrice.ts
+++ src/hooks/useEthPrice.ts
@@ -4,28 +4,31 @@ import { useProvider, useQuery } from 'wagmi'
 
 import AggregatorInterface from '@ensdomains/ens-contracts/build/contracts/AggregatorInterface.json'
 
-import { useChainId } from '@app/hooks/useChainId'
+// import { useChainId } from '@app/hooks/useChainId'
 import { useEns } from '@app/utils/EnsProvider'
 import { useQueryKeys } from '@app/utils/cacheKeyFactory'
 
-const ORACLE_ENS = 'eth-usd.data.eth'
-const ORACLE_GOERLI = '0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e'
+// const ORACLE_ENS = 'eth-usd.data.eth'
+// const ORACLE_GOERLI = '0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e'
+const ORACLE_DUMMY = '0xbb51B0ced6c4261F7d51c3271D577a897BE8eD26'
 
 export const useEthPrice = () => {
   const provider = useProvider()
-  const { getAddr, ready } = useEns()
-  const chainId = useChainId()
+  const { ready } = useEns()
+  // const { getAddr, ready } = useEns()
+  // const chainId = useChainId()
 
   const { data, isLoading: loading } = useQuery(
     useQueryKeys().ethPrice,
     async () => {
-      let address
-      // Goerli
-      if (chainId === 5) {
-        address = ORACLE_GOERLI
-      } else {
-        address = await getAddr(ORACLE_ENS)
-      }
+      // let address
+      // // Goerli
+      // if (chainId === 5) {
+      //   address = ORACLE_GOERLI
+      // } else {
+      //   address = await getAddr(ORACLE_ENS)
+      // }
+      const address = ORACLE_DUMMY
       if (!address) throw new Error('Contract address not found')
       if (typeof address !== 'string') throw new Error('Contract address is wrong type')
       const oracle = new Contract(address, AggregatorInterface, provider)
